c930c254bae995e8f950af0bd0ce56169a774a14
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-10-09 22:25:48 +0900

    chore: did coinflip

--- coin_toss_simulator_dailies/Coin_Toss_4.rb
+++ coin_toss_simulator_dailies/Coin_Toss_4.rb
@@ -1,11 +1,28 @@
-define_method(:toss) do |times=1|
-  Array.new(times) {
-    rand < 0.5 ? "Heads" : "Tails"
-  }
-end
-
 def tui()
-  cmd = gets.chomp
+  current_money = 5000
+  print "\t\t>>> "
+  cmd = gets.chomp.split
+  money = cmd[1].to_i
+  coinflip = rand < 0.5 ? "heads" : "tails"
+  if cmd[0].downcase == "coinflip" || cmd[0].downcase == "cf"
+    if cmd[1].downcase == (rand < 0.5 ? "heads" : "tails") || cmd[1].downcase == (rand < 0.5 ? "h" : "t")
+      puts "You won! you got #{money * 2}"
+    else
+      puts "You lose #{current_money - money}"
+    end
+  else
+    puts "Command: #{cmd[0]} is not a command\ncoinflip help"
+  end
 end
 
-p toss(3)
+def main()
+  while true
+    tui()
+    puts "Continue? y/n\n\t>>> "
+    if ["n", "no"].include?(gets.chomp.downcase)
+      break
+    end
+  end
+end
+
+main()
