08f8a398a9b3032b13d52cf5e3725695f76592be
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-10-12 23:10:07 +0900

    chore: did binary chop

---
+++ binary_chop_dailies/chop11.rb
@@ -0,0 +1,14 @@
+define_method(:bool_rbs) do |u64_vector_list, i32_target|
+  return false unless u64_vector_list.any?
+  u64_midpoint = u64_vector_list.length / 2
+  if u64_vector_list[u64_midpoint] == i32_target
+    return true
+  else
+    if u64_vector_list[u64_midpoint] >= i32_target
+      bool_rbs(u64_vector_list[0...u64_midpoint], i32_target)
+    else
+      bool_rbs(u64_vector_list[(u64_midpoint+1..-1)], i32_target)
+    end
+  end
+
+end
