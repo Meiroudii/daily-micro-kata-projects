683a814ec9f5327115d661435c44eb6d87ec1f7c
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-11-28 19:58:00 +0900

    chore: add console ui

---
+++ SUN/console_gui_dailies/console_ui_3.java
@@ -0,0 +1,19 @@
+class calculator {
+  private int $x;
+  private int $y;
+  String $message = "";
+  String $display_x;
+  String $display_y;
+  String $display_operator;
+  String $display_result;
+
+  public void ui {
+    $String.valueof($x);
+    System.out.printf("""
+      \t\t %d
+    """);
+  }
+}
+
+class console_ui_3 {
+}
