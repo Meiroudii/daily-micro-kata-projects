2ec88b9ff71c2f6171d0c8c5c98a1e09b21fa952
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-12-05 15:10:09 +0900

    chore: did guessing game

--- SAT/guessing_game_dailies/src/main.rs
+++ SAT/guessing_game_dailies/src/main.rs
@@ -1,40 +1,6 @@
-use std::cmp::Ordering;
-use std::io; // This bad boi holds the input/output library
-use rand::Rng;
-use rand::prelude::*;
-
+mod guess_1;
+mod guess_2;
 
 fn main() {
-    if rand::random() {
-        println!("char: {}", rand::random::<char>());
-    }
-    let mut rng = rand::thread_rng();
-    let y: f64 = rng.r#gen();
-    let mut nums: Vec<i32> = (1..100).collect();
-    nums.shuffle(&mut rng);
-    println!("{}", y);
-    println!("{:?}",nums);
-
-    println!("Guess the Number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Cannot read the line.");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed: {guess}");
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too smal!l"),
-            Ordering::Greater => println!("Too large!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    guess_2::main();
 }
