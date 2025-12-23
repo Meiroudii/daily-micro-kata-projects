9dc682741fc5b277e9363e521b9f6bf60186bc72
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-12-24 02:01:05 +0900

    did dailies

---
+++ FRI/blog_page_dailies/blog6/app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+  def index
+    @posts = Post.all
+  end
+  def show
+  end
+  def new
+    @post = Post.new
+  end
+  def edit
+  end
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @post.destroy!
+    respond_to do |format|
+      format.html { redirect_to posts_path, status: :see_other}
+    end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params.expect(:id))
+  end
+  def post_params
+    params.expect(post: [ :title, :content ])
+  end
+end
